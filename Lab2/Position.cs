13b0a1dd6495b250a29c6457e87e41b4644030a5
bejan <beyhanbresner@gmail.com>
2019-11-20 15:14:42 +0100

    added lab

---
+++ Lab2/Position.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Lab2
+{
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double Length()
+        {
+            int x2 = X * X;
+            int y2 = Y * Y;
+
+            return Math.Sqrt(x2 + y2);
+        }
+
+        public bool Equals(Position p)
+        {
+            if (p.X == X && p.Y == Y)
+                return true;
+            else
+                return false;
+        }
+
+        public Position Clone()
+        {
+            return new Position(X, Y);
+        }
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+
+        public static bool operator >(Position p1, Position p2)
+        {
+            if (p1.Length() > p2.Length())
+            {
+                return true;
+            }
+            else if (p1.Equals(p2))
+            {
+                return p1.X > p2.X;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <(Position p1, Position p2)
+        {
+            return p2 > p1;
+        }
+
+        public static Position operator +(Position p1, Position p2)
+        {
+            int newValueX = p1.X + p2.X;
+            int newValueY = p1.Y + p2.Y;
+
+            Position newPos = new Position(newValueX, newValueY);
+            return newPos;
+        }
+
+        public static Position operator -(Position p1, Position p2)
+        {
+            int newValueX = p1.X - p2.X;
+            int newValueY = p1.Y - p2.Y;
+
+            if (newValueX < 0)
+            {
+                newValueX *= -1;
+            }
+            if (newValueY < 0)
+            {
+                newValueY *= -1;
+            }
+
+            Position newPos = new Position(newValueX, newValueY);
+            return newPos;
+        }
+
+        public static double operator %(Position p1, Position p2)
+        {
+            double newValueX2 = (p1.X - p2.X) * (p1.X - p2.X);
+            double newValueY2 = (p1.Y - p2.Y) * (p1.Y - p2.Y);
+
+            return Math.Sqrt(newValueX2 + newValueY2);
+        }
+    }
+}
---
+++ Lab2/SortedPosList.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2
+{
+    public class SortedPosList
+    {
+        private List<Position> PositionList;
+        public SortedPosList()
+        {
+            PositionList = new List<Position>();
+        }
+
+        public int Count()
+        {
+            return PositionList.Count;
+        }
+
+        public Position this[int key]
+        {
+            get => PositionList[key];
+        }
+
+        public void Add(Position pos)
+        {
+            int count = Count();
+
+            if (count == 0)
+            {
+                PositionList.Add(pos);
+            }
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (pos < PositionList[i] || pos == PositionList[i])
+                    {
+                        PositionList.Insert(i, pos);
+                        break;
+                    }
+                    else if (i == count - 1)
+                    {
+                        PositionList.Add(pos);
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        public bool Remove(Position pos)
+        {
+            int items = Count();
+            for (int i = 0; i < items; i++)
+            {
+                if (pos.Equals(PositionList[i]))
+                {
+                    PositionList.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        //Tittar ifall positionen är inuti cirkeln.
+        public SortedPosList CircleContent(Position centerPos, double radius)
+        {
+            SortedPosList newPosList = new SortedPosList();
+
+            foreach (Position pos in PositionList)
+            {
+                int x = pos.X - centerPos.X;
+                int y = pos.Y - centerPos.Y;
+
+                int powX = x * x;
+                int powY = y * y;
+
+                double posRadius = powX + powY;
+                double centerRadius = radius * radius;
+
+                if (posRadius < centerRadius)
+                {
+                    newPosList.Add(pos.Clone());
+                }
+            }
+            return newPosList;
+        }
+
+        public static SortedPosList operator +(SortedPosList sp1, SortedPosList sp2)
+        {
+            SortedPosList combinedPosList = new SortedPosList();
+
+            for (int i = 0; i < sp1.Count(); i++)
+            {
+                combinedPosList.Add(sp1.PositionList[i]);
+            }
+            for (int i = 0; i < sp2.Count(); i++)
+            {
+                combinedPosList.Add(sp2.PositionList[i]);
+            }
+            return combinedPosList;
+        }
+
+        public static SortedPosList operator -(SortedPosList sp1, SortedPosList sp2)
+        {
+            SortedPosList newList = new SortedPosList();
+            int sp1Value = 0;
+            int sp2Value = 0;
+
+            while (sp1Value < sp1.Count() && sp2Value < sp2.Count())
+            {
+                if (sp1[sp1Value] < sp2[sp2Value])
+                {
+                    newList.Add(sp1[sp1Value]);
+                    sp1Value++;
+                }
+                else if (sp1[sp1Value] > sp2[sp2Value])
+                {
+                    sp2Value++;
+                }
+                else
+                {
+                    sp1Value++;
+                    sp2Value++;
+                }
+
+            }
+            return newList;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", PositionList);
+        }
+    }
+}
